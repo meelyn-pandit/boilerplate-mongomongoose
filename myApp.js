f17c8f061d1d4ef9b4d239ed93284c6b33d97c95
Mark Laufersweiler <meelyn.pandit@gmail.com>
2023-12-28 19:25:27 -0500

    added uri with process.env

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
+let mongoose = require('mongoose')
 
 
 let Person;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
